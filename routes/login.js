a480e34607dfe05039da6e30905779c83fd976ff
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-18 18:02:46 +0000

    switching to follow routes used in skeleton

--- routes/login.js
+++ routes/login.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const loginQueries = require('../db/queries/login-queries');
+
+router.get('/:id', (req, res) => {
+  loginQueries.userLogin(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+})
+
+module.exports = router;
