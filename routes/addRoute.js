4e8cf27168edc176e306999eb8cb8413abc75a79
Lisa Cormier <lcormier5@hotmail.com>
2021-09-20 17:19:51 +0000

    working on fetchApi

---
+++ routes/addRoute.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router  = express.Router();
+
+const queryString = `INSERT INTO tasks(user_id, category_type, name, description)
+                    VALUES ($1, $2, $3, $4)`;
+const description = 'Wanting to ';
+const addTask = (db) => {
+  router.post('/', (req, res) => {
+    console.log('task', req.body.task);
+    console.log('user_id', req.session.user_id)
+    const user_id = req.session.user_id;
+    const queryText = req.body.task;
+    if (queryText.includes('watch')) {
+      db.query(`${queryString}`, [user_id, 'movie', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (queryText.includes('read')) {
+      db.query(`${queryString}`, [user_id, 'book', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (queryText.includes('eat')) {
+      db.query(`${queryString}`, [user_id, 'restaurant', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (queryText.includes('buy')) {
+      db.query(`${queryString}`, [user_id, 'products', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (results[0] === 'movie') {
+      db.query(`${queryString}`, [user_id, 'movie', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (results[0] === 'book') {
+      db.query(`${queryString}`, [user_id, 'book', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (results[0] === 'restaurant') {
+      db.query(`${queryString}`, [user_id, 'restaurant', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (results[0] === 'product') {
+      db.query(`${queryString}`, [user_id, 'products', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+    if (results[0] === 'nocat') {
+      db.query(`${queryString}`, [user_id, 'nocat', queryText, `${description} ${queryText}`])
+      .then((response) => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }
+
+  })
+  return router;
+}
+
+module.exports = addTask;
