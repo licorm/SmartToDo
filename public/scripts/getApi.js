0d06c8ea2a89e0a86bd9740c15819d82a5e172b3
Lisa Cormier <lcormier5@hotmail.com>
2021-09-21 00:11:43 +0000

    refractoring API

--- public/scripts/getApi.js
+++ public/scripts/getApi.js
@@ -69,98 +69,6 @@ const wolframSettings = {
 };
 
 
-const fetchYelp = function() {
-  const restaurantResults = [];
- return $.ajax(yelpSettings)
- .done(function (response) {
-
-  let i = 0;
-  while (i < 6) {
-    restaurantResults.push(response.businesses[i].name)
-    i++
-  }
-  for (const element of restaurantResults) {
-
-    const isRestaurant = element.toString()
-
-
-    if (isRestaurant === queryText) {
-      results.push('restaurant');
-
-      return results;
-    }
-  }
-
-    console.log("yelp response:", response);
-  })
-
-}
-
-const failureCallback = (error) => {
-  console.log(error)
-  return
-}
-
-const fetchDandelion = function() {
-
-    let dandelionResults = []
- return $.ajax(dandelionSettings)
-  .done(function (response) {
-
-    console.log("Dandelion Response:", response);
-    let i = 0;
-    while (i < 6) {
-      dandelionResults.push(response.annotations[0].categories[i])
-      i++
-    }
-    if (dandelionResults.length > 0) {
-      for (const element of dandelionResults) {
-        const string = element.toString()
-        const isBook = string.includes('novel')
-        const isMovie = string.includes('film')
-        const isTv = string.includes('television')
-
-        if (isBook) {
-          results.push('book')
-
-          return results;
-        }
-        if (isMovie || isTv) {
-          results.push('movie')
-
-          return results;
-        }
-      }
-    }
-
-  })
-
-
-
-}
-
-const fetchWolfram = function() {
-
-    return $.ajax(wolframSettings)
-  .done(function (response) {
-     console.log('wolfram response:', response.queryresult.datatypes)
-     const dataType = response.queryresult.datatypes
-       if (dataType.toLowerCase() === 'expandedfood' || dataType.toLowerCase() === 'ConsumerProductsPTE') {
-       results.push('product')
-
-       return results;
-     }
-
-     if (dataType.toLowerCase().includes('movie')) {
-       results.push('movie')
-
-       return results;
-     }
-
-
-   })
-
- }
 
 
 
@@ -192,9 +100,7 @@ determineCategory()
 
 
 })
-.done(() => {
-  $("#task").empty();
-})
+
 
 
 
