d9883317eca08976dae2d1709c04f47b854d676c
Lisa Cormier <lcormier5@hotmail.com>
2021-09-21 00:26:44 +0000

    refractoring API statements:

--- public/scripts/apiIndex.js
+++ public/scripts/apiIndex.js
@@ -1,8 +1,10 @@
 const fetchYelp = function() {
   const restaurantResults = [];
+
  return $.ajax(yelpSettings)
  .done(function (response) {
-
+   //check that the response is long enough to iterate
+ if (response.businesses.length > 1) {
   let i = 0;
   while (i < 6) {
     restaurantResults.push(response.businesses[i].name)
@@ -19,55 +21,57 @@ const fetchYelp = function() {
       return results;
     }
   }
+ }
+ console.log("yelp response:", response);
+  return results;
 
-    console.log("yelp response:", response);
   })
-
 }
 
 
 
 const fetchDandelion = function() {
 
-    let dandelionResults = []
- return $.ajax(dandelionSettings)
+  let dandelionResults = []
+  return $.ajax(dandelionSettings)
   .done(function (response) {
-
     console.log("Dandelion Response:", response);
-    let i = 0;
-    while (i < 6) {
-      dandelionResults.push(response.annotations[0].categories[i])
-      i++
-    }
-    if (dandelionResults.length > 0) {
-      for (const element of dandelionResults) {
-        const string = element.toString()
-        const isBook = string.includes('novel')
-        const isMovie = string.includes('film')
-        const isTv = string.includes('television')
+    //check that the response is long enought to iterate
+    if (response.annotations.length > 1) {
+      let i = 0;
+      while (i < 6) {
+        dandelionResults.push(response.annotations[0].categories[i])
+        i++
+      }
+      if (dandelionResults.length > 0) {
+        for (const element of dandelionResults) {
+          const string = element.toString()
+          const isBook = string.includes('novel')
+          const isMovie = string.includes('film')
+          const isTv = string.includes('television')
+          //checks results arrray for film
+          if (isBook) {
+            results.push('book')
 
-        if (isBook) {
-          results.push('book')
+            return results;
+          }
+          //checks the results array for movie
+          if (isMovie || isTv) {
+            results.push('movie')
 
-          return results;
-        }
-        if (isMovie || isTv) {
-          results.push('movie')
-
-          return results;
+            return results;
+          }
         }
       }
     }
 
   })
 
-
-
 }
 
 const fetchWolfram = function() {
 
-    return $.ajax(wolframSettings)
+  return $.ajax(wolframSettings)
   .done(function (response) {
      console.log('wolfram response:', response.queryresult.datatypes)
      const dataType = response.queryresult.datatypes
@@ -82,7 +86,8 @@ const fetchWolfram = function() {
 
        return results;
      }
-
+    //return an empty array if no conditions are passed
+    return results;
 
    })
 
@@ -96,10 +101,10 @@ const fetchWolfram = function() {
    })
    .then(fetchWolfram)
    .then(() => {
+     //if no results were recorded, add no category as a category to negate the non null
      if (results.length === 0 || results.length > 1)
      results.push('nocat')
      console.log(results)
      return results;
-
    })
  }
--- public/scripts/getApi.js
+++ public/scripts/getApi.js
@@ -1,7 +1,6 @@
-
-
 $(() => {
 
+//create form
 const $submitForm = $(`
 <form class="createForm" method="POST" action="/">
       <label class="sr-only">Name</label>
@@ -9,20 +8,23 @@ const $submitForm = $(`
       <button type="submit" class="btn btn-primary mb-2">Add</button>
       </form>
   `)
- const results = [];
 
+//append to main
  const $main = $('#submitform');
  $submitForm.appendTo($main);
 
+ //create array for pushing results
+ const results = [];
 
  $submitForm.on('submit', function(event) {
   event.preventDefault();
 
-
   const data = $(this).parents().children().find('#task').serialize();
-  const queryText = data.slice(5)
-  const unencode = decodeURI(queryText)
-  console.log(unencode)
+  const queryText = data.slice(5);
+  //create unencoded data that can be sent to the database
+  const unencode = decodeURI(queryText);
+  console.log(unencode);
+  //no need to run through API
   if (queryText.includes('watch')) {
     results.push('movie')
     submitTask({
@@ -44,66 +46,39 @@ const $submitForm = $(`
       "task": unencode,
       "category": results[0] });
   } else {
+    //not user specified, must run through APIs
     //yelp settings
-const yelpSettings = {
-	"url": `https://api.yelp.com/v3/businesses/search?term=${queryText}&location=calgary`,
-	"method": "GET",
-	"headers": {
-		"Authorization": "Bearer nxBY2qRdQtx6tQSmpDNElKsuUINdEi_aI_4RDjjvqs3lbzGmgMem__btNaNnT2ruHn28UmFZ1W6Z9zrmjpw0rmyyaEuwGGMc-GSVXD6Q_ffREboy1bP4Po1S6AdGYXYx"
-	}
-};
+  const yelpSettings = {
+	  "url": `https://api.yelp.com/v3/businesses/search?term=${queryText}&location=calgary`,
+   	"method": "GET",
+	  "headers": {
+	   	"Authorization": "Bearer nxBY2qRdQtx6tQSmpDNElKsuUINdEi_aI_4RDjjvqs3lbzGmgMem__btNaNnT2ruHn28UmFZ1W6Z9zrmjpw0rmyyaEuwGGMc-GSVXD6Q_ffREboy1bP4Po1S6AdGYXYx"
+  	}
+  };
 
-// dandelionSettings
-const dandelionSettings = {
-	"async": true,
-	"crossDomain": true,
-	"url": `https://api.dandelion.eu/datatxt/nex/v1/?text=${queryText}&include=types%2Cabstract%2Ccategories&token=aa891623e9ff4f11997a4106ecace392`,
-	"method": "GET"
-};
+  // dandelionSettings
+  const dandelionSettings = {
+  	"async": true,
+  	"crossDomain": true,
+  	"url": `https://api.dandelion.eu/datatxt/nex/v1/?text=${queryText}&include=types%2Cabstract%2Ccategories&token=aa891623e9ff4f11997a4106ecace392`,
+    	"method": "GET"
+  };
 
-//wolframSettings
-const wolframSettings = {
-  "url": `http://api.wolframalpha.com/v2/query?appid=54X4Q5-GJT5YVU638&output=json&input=${queryText}`,
+  //wolframSettings
+  const wolframSettings = {
+    "url": `http://api.wolframalpha.com/v2/query?appid=54X4Q5-GJT5YVU638&output=json&input=${queryText}`,
+    "method": "GET",
+  };
 
-  "method": "GET",
-};
-
-
-
-
-
- const determineCategory = function() {
-  return fetchYelp()
-   .then(fetchDandelion)
-   .catch(error => {
-    return;
+//run through determine category and post results to database
+  determineCategory()
+  .then(() => {
+    submitTask({
+      "task": unencode,
+      "category": results[0] });
    })
-   .then(fetchWolfram)
-   .then(() => {
-     if (results.length === 0 || results.length > 1)
-     results.push('nocat')
-     console.log(results)
-     return results;
-
-   })
- }
-
-determineCategory()
-.then(() => {
-  submitTask({
-    "task": unencode,
-    "category": results[0] });
-})
-
-
   }
-
-
 })
-
-
-
-
 
 
 });
