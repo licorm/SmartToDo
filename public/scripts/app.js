c9f503380d64a4997467057a70471ddd58cbe73b
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-21 04:48:58 +0000

    Add tasks dynamicly from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,35 @@ $(() => {
   //   }
   // });
   $.get('/tasks', function(data) {
-    console.log(data);
+    console.log(data)
+    // console.log(data);
+  //   const taskName = $(`<li class="list-group-item"><div class="form-check">
+  //   <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  //   Shampoo
+  // </div>
+  // <i class="far fa-trash-alt"></i></li>`);
+  for(const obj of data){
+    const taskName = $(`<li class="list-group-item"><div class="form-check">
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    ${obj.name}
+  </div>
+  <i class="far fa-trash-alt"></i></li>`);
+    
+    const type = obj.category_type;
+    if(type === 'books'){
+      $('#books').append(taskName);
+    }else if(type === 'movie'){
+      $('#movie').append(taskName )
+    }else if(type === 'restaurants'){
+      $('#restaurants').append(taskName )
+    }else if(type === 'products'){
+      $('#products').append(taskName )
+    }
+
+
+  }
+
+
+
   });
 });
--- routes/getTasks.js
+++ routes/getTasks.js
@@ -9,36 +9,36 @@ const getTasks = (db) => {
     db.query(query)
       .then(data => {
         const categoties = data.rows;
-        const groupedTasks = {};
-        for(const task of categoties){
-          if(task.category_type === "books"){
-            if(groupedTasks[task.category_type]){
-              groupedTasks[task.category_type].push(task);
-            }else{
-              groupedTasks[task.category_type] = [task];
-            }
-          }else if(task.category_type === "movie"){
-            if(groupedTasks[task.category_type]){
-              groupedTasks[task.category_type].push(task);
-            }else{
-              groupedTasks[task.category_type] = [task];
-            }
-          }else if(task.category_type === "products"){
-            if(groupedTasks[task.category_type]){
-              groupedTasks[task.category_type].push(task);
-            }else{
-              groupedTasks[task.category_type] = [task];
-            }
-          }else if(task.category_type === "restaurants"){
-            if(groupedTasks[task.category_type]){
-              groupedTasks[task.category_type].push(task);
-            }else{
-              groupedTasks[task.category_type] = [task];
-            }
-          }
-      }
+      //   const groupedTasks = {};
+      //   for(const task of categoties){
+      //     if(task.category_type === "books"){
+      //       if(groupedTasks[task.category_type]){
+      //         groupedTasks[task.category_type].push(task);
+      //       }else{
+      //         groupedTasks[task.category_type] = [task];
+      //       }
+      //     }else if(task.category_type === "movie"){
+      //       if(groupedTasks[task.category_type]){
+      //         groupedTasks[task.category_type].push(task);
+      //       }else{
+      //         groupedTasks[task.category_type] = [task];
+      //       }
+      //     }else if(task.category_type === "products"){
+      //       if(groupedTasks[task.category_type]){
+      //         groupedTasks[task.category_type].push(task);
+      //       }else{
+      //         groupedTasks[task.category_type] = [task];
+      //       }
+      //     }else if(task.category_type === "restaurants"){
+      //       if(groupedTasks[task.category_type]){
+      //         groupedTasks[task.category_type].push(task);
+      //       }else{
+      //         groupedTasks[task.category_type] = [task];
+      //       }
+      //     }
+      // }
 
-        res.json(groupedTasks);
+        res.json(categoties);
       })
       .catch(err => {
         res
