3256adcf208be7b1f4fe33d811ef66d10beee4eb
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-21 23:30:56 +0000

    Add a delete request for tasks

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,50 @@
 $(() => {
 
-  $(".userInfo").hide(); // Hides the errorMessage Div
+    $(".userInfo").hide(); // Hides the userInfo Div
 
-  $('.expandButton').click(function(){  // UserIngo form slides , when the icon is clicked 
-    $('.userInfo').toggle('slow');
-});
+    $('.expandButton').click(function(){  // UserIngo form shows , when the plus icon is clicked 
+      $('.userInfo').toggle('slow');
+  });
 
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });
 
-  $.get('/tasks', function(data) {
- 
-  for(const obj of data){
-    const taskName = $(`<li class="list-group-item"><div class="form-check">
-    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-    ${obj.name}
-  </div>
-  <i class="far fa-trash-alt"></i></li>`);
-    
-    const type = obj.category_type;
-    if(type === 'books'){
-      $('#books').append(taskName);
-    }else if(type === 'movie'){
-      $('#movie').append(taskName );
-    }else if(type === 'restaurants'){
-      $('#restaurants').append(taskName);
-    }else if(type === 'products'){
-      $('#products').append(taskName);
-    }else{
-      $('#nocat').append(taskName);
-    }
-  }
+
+    $.get('/tasks', function(data) {
+      for(const obj of data){
+        const taskName = $(`<li class="list-group-item"><div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        ${obj.name}
+      </div>
+      <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
+        
+        const type = obj.category_type;
+        if(type === 'book'){
+          $('#books').append(taskName);
+        }else if(type === 'movie'){
+          $('#movie').append(taskName );
+        }else if(type === 'restaurants'){
+          $('#restaurants').append(taskName);
+        }else if(type === 'product'){
+          $('#products').append(taskName);
+        }else{
+          $('#nocat').append(taskName);
+        }
+      }
+      });
+
+  
+$(document).on("click", ".deleteButton", function(){
+  const $taskToDelete = $(this).parent();
+
+  $.ajax({
+    url: `/delete`,
+    data: {taskId: $(this).attr("id")},
+    method: 'POST',
+    success: function(data) {
+      $taskToDelete.hide();
+    } 
   });
 });
+
+});
+
+
