9e0cd132e5ce9ddba8fb9566fc79eff3e4c548b2
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-22 22:28:14 +0000

    fix categories cards opening

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,50 @@
 $(() => {
+
   $(".userInfo").hide(); // Hides the userInfo Div
-  $('.expandButton').click(function () {  // UserIngo form shows , when the plus icon is clicked 
+  $('.expandButton').click(function () {  // UserIngo form shows , when the plus icon is clicked
     $('.userInfo').toggle('slow');
   });
 
-  $.get('/tasks', function (data) {
-    const countObj = {   
+
+  //function for fetching tweets from /tweets
+  const loadTasks = function() {
+
+
+
+    $.ajax("/tasks", { method: "GET" })
+      .then(function(data) {
+
+
+    const countObj = {
       books: 0,
       movie: 0,
       products: 0,
-      restaurants: 0,
+      restaurant: 0,
       nocat: 0
-    }
+      }
 
+  for(const obj of data){
+    console.log(obj)
+    //change id so that it's not repeated
+
+  if (obj.completed === false) {
     for (const obj of data) {
-      const taskName = $(`<li class="list-group-item"><div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        ${obj.name}
-      </div>
-      <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
-      const type = obj.category_type;
-      if (type === 'book') {
+    const taskName = $(`<li class="list-group-item"><div class="form-check" >
+    <input class="form-check-input" type="checkbox"  id="flexCheckDefault" data-id=${obj.id}>
+    ${obj.name}
+  </div>
+  <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
+    const type = obj.category_type;
+
+      if (type === 'books') {
         $('#books').append(taskName);
         countObj.books += 1;
       } else if (type === 'movie') {
         $('#movie').append(taskName);
         countObj.movie += 1;
-      } else if (type === 'restaurants') {
+      } else if (type === 'restaurant') {
         $('#restaurants').append(taskName);
-        countObj.restaurants += 1;
+        countObj.restaurant += 1;
       } else if (type === 'product') {
         $('#products').append(taskName);
         countObj.products += 1;
@@ -36,15 +52,32 @@ $(() => {
         $('#nocat').append(taskName);
         countObj.nocat += 1;
       }
-    }
-// Updates the count of uncompleted tasks 
-    $(".resCount").text(countObj.books);
-    $(".movieCount").text(countObj.movie);
-    $(".booksCount").text(countObj.restaurants);
-    $(".productsCount").text(countObj.products);
-    $(".nocatCount").text(countObj.nocat);
-  });
 
+// Updates the count of uncompleted tasks
+
+    $("#bookUncomplete").text(countObj.books);
+    $("#movieUncomplete").text(countObj.movie);
+    $("#restaurantUncomplete").text(countObj.restaurant);
+    $("#productsUncomplete").text(countObj.products);
+    $("#nocatUncomplete").text(countObj.nocat);
+
+  }
+}
+    }
+   })
+
+}
+
+  loadTasks();
+
+
+  $(document).on('change', '#flexCheckDefault' , function(event) {
+    console.log(event.target.dataset.id)
+    markComplete({'id': event.target.dataset.id})
+
+    $(this).parent().css("color", "red");
+    $(this).parent().css("text-decoration", "line-through");
+  })
 
   $(document).on("click", ".deleteButton", function () {
     const $taskToDelete = $(this).parent();
@@ -57,6 +90,7 @@ $(() => {
       }
     });
   });
+
 
 });
 
