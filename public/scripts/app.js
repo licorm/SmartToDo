0dcae6aa01334c08ba9e706017fa5227c80cd6c4
Lisa Cormier <lcormier5@hotmail.com>
2021-09-22 16:56:12 +0000

    adding logic for counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+
+
 $(() => {
 
   $(".userInfo").hide(); // Hides the userInfo Div
@@ -5,22 +7,19 @@ $(() => {
     $('.userInfo').toggle('slow');
   });
 
+  const countObj = {
+    book: 0,
+    movie: 0,
+    product: 0,
+    restaurant: 0,
+    nocat: 0
+  }
   //function for fetching tweets from /tweets
   const loadTasks = function () {
 
 
-
-    $.ajax("/tasks", { method: "GET" })
+    getTask()
       .then(function (data) {
-
-
-        const countObj = {
-          books: 0,
-          movie: 0,
-          products: 0,
-          restaurant: 0,
-          nocat: 0
-        }
 
         for (const obj of data) {
           console.log(obj.completed)
@@ -29,7 +28,7 @@ $(() => {
           if (obj.completed === false) {
 
             const taskName = $(`<li class="list-group-item"><div class="form-check" >
-    <input class="form-check-input" type="checkbox"  id="flexCheckDefault" data-id=${obj.id}>
+    <input class="form-check-input" type="checkbox"  id="flexCheckDefault" data-id=${obj.id} data-type=${obj.category_type}>
     ${obj.name}
   </div>
   <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
@@ -38,7 +37,7 @@ $(() => {
 
             if (type === 'book') {
               $('#books').append(taskName);
-              countObj.books += 1;
+              countObj.book += 1;
             } else if (type === 'movie') {
               $('#movie').append(taskName);
               countObj.movie += 1;
@@ -47,39 +46,47 @@ $(() => {
               countObj.restaurant += 1;
             } else if (type === 'product') {
               $('#products').append(taskName);
-              countObj.products += 1;
+              countObj.product += 1;
             } else {
               $('#nocat').append(taskName);
               countObj.nocat += 1;
             }
-
-
-
-            // Updates the count of uncompleted tasks
-
-            $("#bookUncomplete").text(countObj.books);
-            $("#movieUncomplete").text(countObj.movie);
-            $("#restaurantUncomplete").text(countObj.restaurant);
-            $("#productsUncomplete").text(countObj.products);
-            $("#nocatUncomplete").text(countObj.nocat);
+          // Updates the count of uncompleted tasks
+        let $bookCount = $("#bookUncomplete");
+        $bookCount.val(0 + countObj.book);
+        let $movieCount = $("#movieUncomplete");
+        $movieCount.val(0 + countObj.movie)
+        let $restaurantCount = $("#restaurantUncomplete");
+        $restaurantCount.val(0 + countObj.restaurant)
+        let $productCount = $("#productsUncomplete");
+        $productCount.val(0 + countObj.product)
+        let $nocatCount = $("#nocatUncomplete");
+        $nocatCount.val(0 + countObj.nocat)
+        console.log($productCount.val())
 
 
           }
         }
+
       })
 
+
   }
+
 
   loadTasks();
 
 
+
+
   $(document).on('change', '#flexCheckDefault', function (event) {
-    console.log(event.target.dataset.id)
+    console.log(event.target.dataset.type)
 
     markComplete({ 'id': event.target.dataset.id })
       .then(() => {
         $(this).parents().eq(1).hide();
-        loadTasks();
+
+
 
       })
 
@@ -89,16 +96,11 @@ $(() => {
 
   $(document).on("click", ".deleteButton", function () {
     const $taskToDelete = $(this).parent();
-    $.ajax({
-      url: `/delete`,
-      data: { taskId: $(this).attr("id") },
-      method: 'POST',
-      success: function (data) {
-        $taskToDelete.hide();
-      }
-    })
+
+    deleteTask({ taskId: $(this).attr("id") })
     .then(() => {
-      loadTasks()
+      $taskToDelete.hide();
+
     });
   });
 
