7aac01b725725affebe51daa3fe21f03c7cbf818
Lisa Cormier <lcormier5@hotmail.com>
2021-09-22 23:36:11 +0000

    changing drag to find child node

--- public/scripts/drag.js
+++ public/scripts/drag.js
@@ -8,7 +8,7 @@ $(() => {
     $( ".list-group" ).sortable({
         connectWith: ".connectedSortable",
         start: function( event, ui ) {
-          console.log($(this).children().eq(1).find(".form-check-input")[0].dataset)
+          console.log($(this).children().find(".form-check-input")[0].dataset)
         },
         receive: function(event, ui) {
           console.log(event.target.dataset.category)
