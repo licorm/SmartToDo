d88974b5e20cb2456e96c9949c9a0c5e59f0943f
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-09-22 00:12:41 +0000

    Add count of unfinished tasks

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,49 +1,62 @@
 $(() => {
-
-    $(".userInfo").hide(); // Hides the userInfo Div
-
-    $('.expandButton').click(function(){  // UserIngo form shows , when the plus icon is clicked 
-      $('.userInfo').toggle('slow');
+  $(".userInfo").hide(); // Hides the userInfo Div
+  $('.expandButton').click(function () {  // UserIngo form shows , when the plus icon is clicked 
+    $('.userInfo').toggle('slow');
   });
 
+  $.get('/tasks', function (data) {
+    const countObj = {   
+      books: 0,
+      movie: 0,
+      products: 0,
+      restaurants: 0,
+      nocat: 0
+    }
 
-
-    $.get('/tasks', function(data) {
-      for(const obj of data){
-        const taskName = $(`<li class="list-group-item"><div class="form-check">
+    for (const obj of data) {
+      const taskName = $(`<li class="list-group-item"><div class="form-check">
         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
         ${obj.name}
       </div>
       <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
-        
-        const type = obj.category_type;
-        if(type === 'book'){
-          $('#books').append(taskName);
-        }else if(type === 'movie'){
-          $('#movie').append(taskName );
-        }else if(type === 'restaurants'){
-          $('#restaurants').append(taskName);
-        }else if(type === 'product'){
-          $('#products').append(taskName);
-        }else{
-          $('#nocat').append(taskName);
-        }
+      const type = obj.category_type;
+      if (type === 'book') {
+        $('#books').append(taskName);
+        countObj.books += 1;
+      } else if (type === 'movie') {
+        $('#movie').append(taskName);
+        countObj.movie += 1;
+      } else if (type === 'restaurants') {
+        $('#restaurants').append(taskName);
+        countObj.restaurants += 1;
+      } else if (type === 'product') {
+        $('#products').append(taskName);
+        countObj.products += 1;
+      } else {
+        $('#nocat').append(taskName);
+        countObj.nocat += 1;
       }
-      });
-
-  
-$(document).on("click", ".deleteButton", function(){
-  const $taskToDelete = $(this).parent();
-
-  $.ajax({
-    url: `/delete`,
-    data: {taskId: $(this).attr("id")},
-    method: 'POST',
-    success: function(data) {
-      $taskToDelete.hide();
-    } 
+    }
+// Updates the count of uncompleted tasks 
+    $(".resCount").text(countObj.books);
+    $(".movieCount").text(countObj.movie);
+    $(".booksCount").text(countObj.restaurants);
+    $(".productsCount").text(countObj.products);
+    $(".nocatCount").text(countObj.nocat);
   });
-});
+
+
+  $(document).on("click", ".deleteButton", function () {
+    const $taskToDelete = $(this).parent();
+    $.ajax({
+      url: `/delete`,
+      data: { taskId: $(this).attr("id") },
+      method: 'POST',
+      success: function (data) {
+        $taskToDelete.hide();
+      }
+    });
+  });
 
 });
 
