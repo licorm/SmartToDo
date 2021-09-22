be106fbefa5563f8596e9225952e746ef5036dce
Leland Mckibben <lmckibben28@hotmail.com>
2021-09-22 03:38:48 +0000

    grabbed from master branch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,52 +1,75 @@
 $(() => {
   $(".userInfo").hide(); // Hides the userInfo Div
-  $('.expandButton').click(function () {  // UserIngo form shows , when the plus icon is clicked 
+  $('.expandButton').click(function () {  // UserIngo form shows , when the plus icon is clicked
     $('.userInfo').toggle('slow');
   });
 
-  $.get('/tasks', function (data) {
-    const countObj = {   
-      book: 0,
+  //function for fetching tweets from /tweets
+  const loadTasks = function() {
+
+
+
+    $.ajax("/tasks", { method: "GET" })
+      .then(function(data) {
+
+
+    const countObj = {
+      books: 0,
       movie: 0,
       products: 0,
       restaurant: 0,
       nocat: 0
-    }
+      }
 
+  for(const obj of data){
+    console.log(obj)
+    //change id so that it's not repeated
 
+  if (obj.completed === false) {
     for (const obj of data) {
-      const taskName = $(`<li class="list-group-item"><div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        ${obj.name}
-      </div>
-      <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
-      const type = obj.category_type;
+    const taskName = $(`<li class="list-group-item"><div class="form-check" >
+    <input class="form-check-input" type="checkbox"  id="flexCheckDefault" data-id=${obj.id}>
+    ${obj.name}
+  </div>
+  <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
+    const type = obj.category_type;
+
+
       if (type === 'book') {
         $('#books').append(taskName);
-        countObj.book += 1;
+        countObj.books += 1;
       } else if (type === 'movie') {
         $('#movie').append(taskName);
         countObj.movie += 1;
       } else if (type === 'restaurant') {
         $('#restaurants').append(taskName);
         countObj.restaurant += 1;
-      } else if (type === 'products') {
+      } else if (type === 'product') {
         $('#products').append(taskName);
         countObj.products += 1;
       } else {
         $('#nocat').append(taskName);
         countObj.nocat += 1;
       }
-    }
-    
-// Updates the count of uncompleted tasks 
-    $(".resCount").text(countObj.books);
-    $(".movieCount").text(countObj.movie);
-    $(".booksCount").text(countObj.restaurants);
-    $(".productsCount").text(countObj.products);
-    $(".nocatCount").text(countObj.nocat);
-  });
 
+
+
+// Updates the count of uncompleted tasks
+
+    $("#bookUncomplete").text(countObj.books);
+    $("#movieUncomplete").text(countObj.movie);
+    $("#restaurantUncomplete").text(countObj.restaurant);
+    $("#productsUncomplete").text(countObj.products);
+    $("#nocatUncomplete").text(countObj.nocat);
+
+  }
+}
+    }
+   })
+
+}
+
+  loadTasks();
 
   $(document).on("click", ".deleteButton", function () {
     const $taskToDelete = $(this).parent();
@@ -59,6 +82,8 @@ $(() => {
       }
     });
   });
+
+  
 
 });
 
