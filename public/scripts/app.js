7ced28e17f399a4bbfb961a7562b079db63feddb
Lisa Cormier <lcormier5@hotmail.com>
2021-09-22 02:53:30 +0000

    removing completed tasks from list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,76 +6,82 @@ $(() => {
   });
 
   //function for fetching tweets from /tweets
-  const loadTasks = function() {
+  const loadTasks = function () {
 
 
 
     $.ajax("/tasks", { method: "GET" })
-      .then(function(data) {
+      .then(function (data) {
 
 
-    const countObj = {
-      books: 0,
-      movie: 0,
-      products: 0,
-      restaurant: 0,
-      nocat: 0
-      }
+        const countObj = {
+          books: 0,
+          movie: 0,
+          products: 0,
+          restaurant: 0,
+          nocat: 0
+        }
 
-  for(const obj of data){
-    console.log(obj)
-    //change id so that it's not repeated
+        for (const obj of data) {
+          console.log(obj.completed)
+          //change id so that it's not repeated
 
-  if (obj.completed === false) {
-    for (const obj of data) {
-    const taskName = $(`<li class="list-group-item"><div class="form-check" >
+          if (obj.completed === false) {
+
+            const taskName = $(`<li class="list-group-item"><div class="form-check" >
     <input class="form-check-input" type="checkbox"  id="flexCheckDefault" data-id=${obj.id}>
     ${obj.name}
   </div>
   <button class="deleteButton" id ="${obj.id}"><i class="far fa-trash-alt"></i></li> </button>`);
-    const type = obj.category_type;
+            const type = obj.category_type;
 
 
-      if (type === 'book') {
-        $('#books').append(taskName);
-        countObj.books += 1;
-      } else if (type === 'movie') {
-        $('#movie').append(taskName);
-        countObj.movie += 1;
-      } else if (type === 'restaurant') {
-        $('#restaurants').append(taskName);
-        countObj.restaurant += 1;
-      } else if (type === 'product') {
-        $('#products').append(taskName);
-        countObj.products += 1;
-      } else {
-        $('#nocat').append(taskName);
-        countObj.nocat += 1;
-      }
+            if (type === 'book') {
+              $('#books').append(taskName);
+              countObj.books += 1;
+            } else if (type === 'movie') {
+              $('#movie').append(taskName);
+              countObj.movie += 1;
+            } else if (type === 'restaurant') {
+              $('#restaurants').append(taskName);
+              countObj.restaurant += 1;
+            } else if (type === 'product') {
+              $('#products').append(taskName);
+              countObj.products += 1;
+            } else {
+              $('#nocat').append(taskName);
+              countObj.nocat += 1;
+            }
 
 
 
-// Updates the count of uncompleted tasks
+            // Updates the count of uncompleted tasks
 
-    $("#bookUncomplete").text(countObj.books);
-    $("#movieUncomplete").text(countObj.movie);
-    $("#restaurantUncomplete").text(countObj.restaurant);
-    $("#productsUncomplete").text(countObj.products);
-    $("#nocatUncomplete").text(countObj.nocat);
+            $("#bookUncomplete").text(countObj.books);
+            $("#movieUncomplete").text(countObj.movie);
+            $("#restaurantUncomplete").text(countObj.restaurant);
+            $("#productsUncomplete").text(countObj.products);
+            $("#nocatUncomplete").text(countObj.nocat);
+
+
+          }
+        }
+      })
 
   }
-}
-    }
-   })
-
-}
 
   loadTasks();
 
 
-  $(document).on('change', '#flexCheckDefault' , function(event) {
+  $(document).on('change', '#flexCheckDefault', function (event) {
     console.log(event.target.dataset.id)
-    markComplete({'id': event.target.dataset.id})
+
+    markComplete({ 'id': event.target.dataset.id })
+      .then(() => {
+        $(this).parents().eq(1).hide();
+        loadTasks();
+
+      })
 
     $(this).parent().css("color", "red");
     $(this).parent().css("text-decoration", "line-through");
@@ -90,6 +96,9 @@ $(() => {
       success: function (data) {
         $taskToDelete.hide();
       }
+    })
+    .then(() => {
+      loadTasks()
     });
   });
 
