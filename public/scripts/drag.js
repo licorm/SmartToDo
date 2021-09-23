834672c26f91eb56396c76792a4556987380bd4a
Lisa Cormier <lcormier5@hotmail.com>
2021-09-23 03:54:30 +0000

    adding logic for task counter

--- public/scripts/drag.js
+++ public/scripts/drag.js
@@ -1,11 +1,56 @@
-$(() => {
 
+$(() => {
+  let $movieCount = $("#movieUncomplete");
+  let $bookCount = $("#bookUncomplete");
+  let $restaurantCount = $("#restaurantUncomplete");
+  let $productCount = $("#productsUncomplete");
+  let $nocatCount = $("#nocatUncomplete");
+  const countTasks = function() {
+    getTask()
+    .then((data) => {
+      let movCount = 0;
+      let booCount = 0;
+      let restCount = 0;
+      let prodCount = 0;
+      let nocCount = 0;
+      for (const task of data) {
+        const type = task.category_type;
+        console.log(type)
+        if (task.completed === false) {
+          if (type === 'book') {
+           booCount += 1;
+
+          } else if (type === 'movie') {
+            movCount += 1;
+
+          } else if (type === 'restaurant') {
+            restCount += 1;
+
+          } else if (type === 'product') {
+            prodCount += 1;
+
+          } else {
+            nocCount += 1;
+
+          }
+        }
+      }
+      $movieCount.val(movCount);
+      $bookCount.val(booCount);
+      $restaurantCount.val(restCount);
+      $productCount.val(prodCount);
+      $nocatCount.val(nocCount);
+
+    })
+
+
+  }
 
 
 
   let id = ""
   $(function() {
-    $( ".list-group" ).sortable({
+    $( ".list-group-task" ).sortable({
 
         connectWith: ".connectedSortable",
         start: function( mousedown, ui ) {
@@ -20,6 +65,7 @@ $(() => {
             "id": id,
             "category": category
           })
+          countTasks();
         }
     })
 
--- routes/addRoute.js
+++ routes/addRoute.js
@@ -42,7 +42,7 @@ const addTask = (db) => {
       wolfram(queryText)
       .then((category) => {
         console.log('category', category)
-        if (category === 'products') {
+        if (category === 'product') {
           categoryString += 'buy '
           db.query(`${queryString}`, [user_id, category, queryText, `${categoryString}${queryText}`])
           .then((data) => {
@@ -83,13 +83,13 @@ const addTask = (db) => {
                 .then((data) => {
                 res.json(data.rows[0]);
                 })
-              } 
+              }
               if (category === 'nocat') {
                 db.query(`${queryString}`, [user_id, category, queryText, `${categoryString}${queryText}`])
                 .then((data) => {
                   res.json(data.rows[0]);
                 })
-              } 
+              }
             })
           }
 
